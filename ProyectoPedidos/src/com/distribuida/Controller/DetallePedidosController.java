90febdf8104a084d575f14184ea857f29bd45b1b
NOCTURNO <NOCTURNO@DESKTOP-IRBQMBP>
2024-08-12 12:32:10 -0500

    V0002 Implementacion del DetalleControle

--- ProyectoPedidos/src/com/distribuida/Controller/DetallePedidosController.java
+++ ProyectoPedidos/src/com/distribuida/Controller/DetallePedidosController.java
@@ -3,8 +3,10 @@ package com.distribuida.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.distribuida.DAO.DetallePedidosDAO;
 import com.distribuida.entities.DetallePedidos;
@@ -19,5 +21,17 @@ public class DetallePedidosController {
 		List<DetallePedidos> detallePedidos = detallePedidosDAO.findAll();
 		model.addAttribute("detallePedidos", detallePedidos);
 		return "etallePedidos-listar";
+	}
+	
+	@GetMapping("/findOne")
+	public String findOne(@RequestParam("idDetalleDedidos")@Nullable Integer idDetalleDedidos
+			             ,@RequestParam("opcion")@Nullable Integer opcion
+			             ,Model model) {
+		if(idDetalleDedidos != null) {
+			DetallePedidos detallePedidos = detallePedidosDAO.findOne(idDetalleDedidos);
+			model.addAttribute("detallePedidos", detallePedidos);
+		}
+		if(opcion == 1) return "detallepedidos-add";
+		else return "detallepedidos-del";
 	}
 }
